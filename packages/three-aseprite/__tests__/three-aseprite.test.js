1760c1581645f815d9ab76a9cf95a471882c9f98
Robert Brownstein <robert.brownstein@gmail.com>
2024-03-09 09:24:41 -0500

    rm unused test dir, update install

--- packages/three-aseprite/__tests__/three-aseprite.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const threeAseprite = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(threeAseprite(), 'Hello from threeAseprite');
-console.info('threeAseprite tests passed');
